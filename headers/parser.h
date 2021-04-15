88964a7ce07a03c104723abe07344be972346c09
Pond Marash <pmarash@un-g6.kzn.21-school.ru>
2021-04-15 16:24:18 +0300

    headers

---
+++ headers/parser.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarash <pmarash@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/13 17:55:33 by pmarash           #+#    #+#             */
+/*   Updated: 2021/04/15 15:44:11 by pmarash          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include <stdbool.h>
+
+typedef struct s_lst{
+	char			*str;
+	struct s_lst	*next;
+}					t_lst;
+
+typedef struct s_status {
+	bool			dbl_qt;
+	bool			qt;
+	int				pipe;
+	int				semicolon;
+}					t_status;
+
+int		environment_to_struct(t_lst **env, char **envp);
+t_lst	*lstnew_char(char *content);
+void	lst_add_back(t_lst **lst, t_lst *new);
+void	lstclear_char(t_lst **lst);
+int		char_join(char **arg, char symbol, int arg_size);
+
+
+#endif
