08c0f9a44dcfbf60d169e5bb9eee9e7287e76cb6
vping <kepovich98@bk.ru>
2021-07-20 16:40:31 +0300

    fixed cd

--- built_in/ft_cd.c
+++ built_in/ft_cd.c
@@ -1,8 +1,17 @@
 #include "../headers/overall.h"
 
+static void print_error(char *new_path)
+{
+	ft_putstr_fd("cd: ", 2);
+	ft_putstr_fd((char *)new_path, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(strerror(g_errno), 2);
+	ft_putstr_fd("\n", 2);
+}
+
 void ft_cd(t_all *all, char *new_path)
 {
-	t_env_list	*tmp;
+	t_env_item	*tmp;
 	char 		*buf;
 	char 		*old_pwd;
 
@@ -11,7 +20,18 @@ void ft_cd(t_all *all, char *new_path)
 	if (chdir(new_path) == -1)
 	{
 		g_errno = errno;
+		print_error(new_path);
 		return ;
 	}
-	tmp = ft_get_env_item
+	g_errno = 0;
+	tmp = ft_get_env_item_with_key(all->env, "PWD");
+	if (!tmp)
+		return ;
+	buf = getcwd(NULL, 0);
+	old_pwd = tmp->value;
+	tmp->value = buf;
+	tmp = ft_get_env_item_with_key(all->env, "OLDPWD");
+	if (!tmp)
+		return (free(old_pwd));
+	tmp->value = old_pwd;
 }
