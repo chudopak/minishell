00393fb1c1ad6cbbb6a787c1f9fdc01c4a7a94b1
pmarash <stepakirillov59@gmail.com>
2021-07-26 19:09:45 +0300

    norm

--- parser/set_fd_for_redirect.c
+++ parser/set_fd_for_redirect.c
@@ -1,32 +1,30 @@
 #include "../headers/overall.h"
 
-int	set_fd(t_command *command, char *redir_type, char *file_name)
+int	set_fd(t_command *com, char *redir_type, char *f_name)
 {
 	if (!ft_strcmp(redir_type, ">>"))
 	{
-		if (command->redirectout != -1)
-			close(command->redirectout);
-		command->redirectout = open(file_name,
-				O_WRONLY | O_APPEND | O_CREAT, S_IRWXU);
-		if (command->redirectout == -1)
-			return (puterror1(file_name, ": ", strerror(errno), errno));
+		if (com->redirectout != -1)
+			close(com->redirectout);
+		com->redirectout = open(f_name, O_WRONLY | O_APPEND | O_CREAT, S_IRWXU);
+		if (com->redirectout == -1)
+			return (puterror1(f_name, ": ", strerror(errno), errno));
 	}
 	else if (!ft_strcmp(redir_type, ">"))
 	{
-		if (command->redirectout != -1)
-			close(command->redirectout);
-		command->redirectout = open(file_name,
-				O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
-		if (command->redirectout == -1)
-			return (puterror1(file_name, ": ", strerror(errno), errno));
+		if (com->redirectout != -1)
+			close(com->redirectout);
+		com->redirectout = open(f_name, O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
+		if (com->redirectout == -1)
+			return (puterror1(f_name, ": ", strerror(errno), errno));
 	}
 	else if (!ft_strcmp(redir_type, "<"))
 	{
-		if (command->redirectin != -1)
-			close(command->redirectin);
-		command->redirectin = open(file_name, O_RDONLY, S_IRWXU);
-		if (command->redirectin == -1)
-			return (puterror1(file_name, ": ", strerror(errno), errno));
+		if (com->redirectin != -1)
+			close(com->redirectin);
+		com->redirectin = open(f_name, O_RDONLY, S_IRWXU);
+		if (com->redirectin == -1)
+			return (puterror1(f_name, ": ", strerror(errno), errno));
 	}
 	return (0);
 }
@@ -50,27 +48,3 @@ int	set_fd_for_redirect(t_lst *args, t_command *command)
 	}
 	return (0);
 }
-
-
-//int	set_fd_for_redirect(t_command *command)
-//{
-//	int		i;
-//	int		status;
-//
-//	i = 0;
-//	while (command->cmd[i])
-//	{
-//		if (!ft_strcmp(command->cmd[i], ">>")
-//			|| !ft_strcmp(command->cmd[i], ">")
-//			|| !ft_strcmp(command->cmd[i], "<"))
-//		{
-//			status = set_fd(command, command->cmd[i], *(command->cmd + i + 1));
-//			if (status)
-//				return (-1);
-//			i++;
-//		}
-//		i++;
-//	}
-//	return (0);
-//}
-//
